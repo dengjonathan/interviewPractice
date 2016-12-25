74e3c772dda6f3a79586c5643f875aabe3d51367
jonathan.deng <dengjonathan@gmail.com>
2016-12-25 16:02:12 -0500

    working on LFU cache

---
+++ LFUcache.js
@@ -0,0 +1,16 @@
+// 3 data structures
+// hash with doubly linked list on each frequency
+// hash with reference to value and memory node
+class LinkedList {
+  
+}
+
+
+class LinkedHashMap {
+  constructor() {
+    this.storage = {};
+  }
+  addBucket(index) {
+
+  }
+} 
