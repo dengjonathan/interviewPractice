a104dadbd3c5aa1fa85046c08554acb76f392214
jonathan.deng <dengjonathan@gmail.com>
2017-01-16 15:12:51 -0800

    added optimized solution

--- commonAncestorII.js
+++ commonAncestorII.js
@@ -41,6 +41,41 @@ class Tree {
   }
 }
 
+
+// below is optimized solution, where we only have to search the tree once
+// if tree contains a but not b, return a
+// if tree contains b but not a, return b
+// if tree contains their common ancestor, return common ancestor
+// if tree contains neither p nor q, return null
+
+const commonAncestorOpt = (root, a, b) => {
+  if (!root) {
+    return null;
+  }
+  if (a === b) {
+    return a;
+  }
+
+  const left = commonAncestorOpt(root.left, a, b);
+  // if left is not null and not a or b, it is the common ancestor
+  if (left && left !== a && left !== b) {
+    return left;
+  }
+
+  const right = commonAncestorOpt(root.right, a, b);
+  if (right && right !==a && right !== b) {
+    return right;
+  }
+
+  // if one is on left and other on right, or either a or b is root
+  if (left && right || root === a || root === b) {
+    return root;
+  } else {
+    // return not null one
+    return left ? left : right;
+  }
+}
+
 const tree = new Tree(4);
 const a = new Tree('a');
 const b = new Tree('b');
@@ -54,4 +89,4 @@ c.left = d;
 c.right = a;
 d.left = b;
 
-expect(commonAncestor(tree, a, b)).to.equal(c)
+expect(commonAncestorOpt(tree, a, e)).to.equal(tree);
