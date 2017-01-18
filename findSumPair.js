bc8b0f27285d8ca2e816b4b2eae10af11fe45a7a
jonathan.deng <dengjonathan@gmail.com>
2017-01-18 14:39:05 -0800

    add unsorted version

--- findSumPair.js
+++ findSumPair.js
@@ -1,5 +1,5 @@
 // find a pair of values in a sorted array equal to a sum
-
+// if sorted:
 const sumInArray = (arr, target) => {
   let left = 0;
   let right = arr.length - 1;
@@ -22,3 +22,20 @@ const expect = require('chai').expect;
 
 expect(sumInArray([1,2,4,4], 8)).to.equal(true);
 expect(sumInArray([1,2,3,9], 8)).to.equal(false);
+
+// if unsorted?
+// use hashing in a single pass
+// O(n) Timke and space
+const sumInArrayUnsorted = (arr, target) => {
+  const set = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(target - arr[i])) {
+      return true;
+    }
+    set.add(arr[i]);
+  }
+  return false;
+};
+
+expect(sumInArrayUnsorted([1,2,4,5], 9)).to.equal(true);
+expect(sumInArrayUnsorted([1,2,3,9], 8)).to.equal(false);
