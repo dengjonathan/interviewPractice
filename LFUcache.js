b277f9c0288d535ca9bfc9885064fed4c5f70fd7
jonathan.deng <dengjonathan@gmail.com>
2017-01-19 22:08:27 -0800

    completed LFU cache

--- LFUcache.js
+++ LFUcache.js
@@ -1,86 +1,50 @@
-// 3 data structures
-// hash with doubly linked list on each frequency
-// hash with reference to value and memory node
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-    this.prev = null;
+const PriorityQueue = require('./PriorityQueue');
+
+class LFUCache {
+  constructor(size) {
+    this.cache = {};
+    this.priorityQueue = new PriorityQueue();
+    this.size = size;
+    this.items = 0;
+  }
+
+  set(key, value) {
+    this.items++;
+    if (this.items > this.size) {
+      const evicted = this.priorityQueue.evict();
+      delete this.cache[evicted];
+      this.items--;
+    }
+    this.cache[key] = value;
+    this.priorityQueue.add(key);
+  }
+
+  get(key) {
+    if (this.cache[key]) {
+      this.priorityQueue.increment(key);
+    }
+    return this.cache[key];
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-  addToTail(node) {
-    if (!this.head) {
-      this.head = this.tail = node;
-    } else {
-      node.prev = this.tail;
-      this.tail.next = node;
-      this.tail = node;
-    }
-    return node;
-  }
-  splice(node) {
-    if (this.head === node) {
-      this.head = node.next // can be null
-    }
-    if (this.tail === node) {
-      this.tail = node.prev; // can be null
-    }
-    if (node.next) {
-      node.next.prev = node.prev;
-    }
-    if (node.prev) {
-      node.prev.Next = node.next;
-    }
-    return node;
-  }
-  toArray() {
-    if (!this.head) {
-      return [];
-    }
-    const result = [];
-    let current = this.head;
-    while (current) {
-      result.push(current.val);
-      current = current.next;
-    }
-    return result;
-  }
-}
-
-// class LinkedHashMap {
-//   constructor() {
-//     this.storage = {};
-//     this.size = 0;
-//   }
-//   addBucket(index) {
-//     if (!this.storage[bucket]) {
-//       this.storage[bucket] = [];
-//     }
-//   }
-//   addValue(key) {
-
-//   }
-// }
-
-// TESTS
 const expect = require('chai').expect;
-const list = new LinkedList();
 
-const refs = [];
+const cache = new LFUCache(3);
 
-[1, 2, 3, 4, 5].forEach(num => {
-  refs.push(list.addToTail(new Node(num)));
-});
+cache.set('hello', 'world');
+cache.set('good', 'bye');
+cache.set('least', 'used');
 
-list.splice(refs[3]);
-list.splice(refs[0]);
-list.splice(refs[refs.length - 1]);
+expect(cache.get('hello')).to.equal('world');
+expect(cache.get('good')).to.equal('bye');
 
-expect(list.toArray()).to.eql([2, 4]);
-console.log('all tests passed!!!!');
+cache.set('new', 'one');
+
+expect(cache.get('least')).to.equal(undefined);
+expect(cache.items).to.equal(3);
+
+expect(cache.get('new')).to.equal('one');
+
+cache.set('last', 'one');
+expect(cache.get('last')).to.equal('one');
+expect(cache.get('hello')).to.equal(undefined);
