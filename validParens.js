395134b58aef93e24e4bd4ddaa03257b1d153f9e
jonathan.deng <dengjonathan@gmail.com>
2017-01-23 12:46:29 -0800

    valid parents

---
+++ validParens.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const complements = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        } else {
+            const opener = stack.pop();
+            console.log(opener, s[i], complements[s[i]])
+            if (!opener || opener !== complements[s[i]]) {
+                return false;
+            }
+        }
+    }
+    return stack.length ? false : true;
+};
